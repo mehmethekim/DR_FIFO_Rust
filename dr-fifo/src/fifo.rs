98cbb94b5192c4ad9ce16bb3c76a82f2b3397e8b
Mehmet Hekimoğlu <mehmethekimolu@gmail.com>
2024-01-21 12:48:11 +0300

    changes

--- dr-fifo/src/fifo.rs
+++ dr-fifo/src/fifo.rs
@@ -1,28 +1,49 @@
 // scheduler.rs
 
 use crate::packet::Packet;
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 use std::time::{Duration, Instant};
 
 // FIFO Scheduler structure
 pub struct FifoScheduler {
-    queue: VecDeque<Packet>,
+    input_queues: HashMap<u32, VecDeque<Packet>>, // Map input port to its queue
+    output_ports: Vec<Option<Packet>>, // The output ports, each can hold at most 1 packet
     served_packets_count: usize,
     last_serve_time: Instant,
 }
 
 impl FifoScheduler {
     pub fn new() -> Self {
+        let mut input_queues = HashMap::new();
+        let mut output_ports = Vec::with_capacity(4);
+        for _ in 0..4 {
+            output_ports.push(None);
+        }
+
+        for i in 0..4 {
+            input_queues.insert(i, VecDeque::new());
+        }
+
         Self {
-            queue: VecDeque::new(),
+            input_queues,
+            output_ports,
             served_packets_count: 0,
             last_serve_time: Instant::now(),
         }
     }
 
     pub fn enqueue(&mut self, packet: Packet) {
-        self.queue.push_back(packet);
-        println!("Enqueued: Packet ID {:?}", self.queue.back().unwrap().id);
+        let input_port = packet.incoming_source;
+        self.input_queues
+            .entry(input_port)
+            .or_insert(VecDeque::new())
+            .push_back(packet.clone());
+        println!(
+            "Enqueued: Packet ID {:?} at Input Port {}",
+            packet.id, input_port
+        );
+
+        self.print_queues();
     }
 
     pub fn serve_packets(&mut self) {
@@ -37,12 +58,46 @@ impl FifoScheduler {
 
         // Serve up to 4 packets if the limit has not been reached
         while self.served_packets_count < 4 {
-            if let Some(packet) = self.queue.pop_front() {
-                println!("Dequeued: Packet ID {:?}", &packet.id);
+            let output_port = self.served_packets_count;
+            if let Some(packet) = self.get_next_packet(output_port) {
+                println!(
+                    "Dequeued: Packet ID {:?} from Input Port {} to Output Port {}",
+                    packet.id, packet.incoming_source, output_port
+                );
+                self.output_ports[output_port] = Some(packet);
                 self.served_packets_count += 1;
             } else {
-                break; // Break if there are no more packets in the queue
+                break; // Break if there are no more packets to serve
             }
         }
+
+        self.print_queues();
+    }
+
+    fn get_next_packet(&mut self, output_port: usize) -> Option<Packet> {
+        // Try to get the next packet from the first non-empty input queue
+        for input_queue in self.input_queues.values_mut() {
+            if let Some(packet) = input_queue.pop_front() {
+                return Some(packet);
+            }
+        }
+
+        // If no packets are available in any input queue, try to get from the output port
+        if let Some(packet) = self.output_ports[output_port].take() {
+            return Some(packet);
+        }
+
+        None
+    }
+
+    fn print_queues(&self) {
+        for (input_port, input_queue) in &self.input_queues {
+            print!("{}st Queue: ", input_port + 1);
+            for packet in input_queue {
+                print!("{},", packet.id);
+            }
+            println!();
+        }
+        println!();
     }
 }
--- dr-fifo/src/packet.rs
+++ dr-fifo/src/packet.rs
@@ -2,20 +2,22 @@
 
 use rand::Rng;
 use rand_distr::{Distribution, Poisson};
+use std::time::{Duration, Instant};
 
 // Packet structure
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Packet {
-    pub id: u32,
+    pub id: u64,
     pub priority: u32,
     pub data: Vec<u8>,
     pub incoming_source: u32,
     pub outgoing_source: u32,
+    pub incoming_time: Instant,
 }
 
 // Packet generator
 pub struct PacketGenerator {
-    packet_id_counter: u32,
+    global_packet_id_counter: u64,
     incoming_sources: usize,
     outgoing_sources: usize,
     poisson_lambda: f64,
@@ -24,7 +26,7 @@ pub struct PacketGenerator {
 impl PacketGenerator {
     pub fn new(incoming_sources: usize, outgoing_sources: usize, poisson_lambda: f64) -> Self {
         Self {
-            packet_id_counter: 0,
+            global_packet_id_counter: 0,
             incoming_sources,
             outgoing_sources,
             poisson_lambda,
@@ -40,20 +42,29 @@ impl PacketGenerator {
         for _ in 0..packets_to_generate {
             let priority = self.generate_priority();
             let data = self.generate_data();
+            let incoming_time = Instant::now();
 
             let packet = Packet {
-                id: self.packet_id_counter,
+                id: self.global_packet_id_counter,
                 priority,
                 data,
                 incoming_source: rand::thread_rng().gen_range(0..self.incoming_sources as u32),
                 outgoing_source: rand::thread_rng().gen_range(0..self.outgoing_sources as u32),
+                incoming_time,
             };
 
-            self.packet_id_counter += 1;
+            self.global_packet_id_counter += 1;
             packets.push(packet);
         }
 
-        println!("Time: {}s - Generated {} packets", time_elapsed, packets.len());
+        // Print the IDs of the enqueued packets
+        let enqueued_ids: Vec<_> = packets.iter().map(|packet| packet.id).collect();
+        println!(
+            "Time: {}s - Generated {} packets. Enqueued Packet IDs: {:?}",
+            time_elapsed,
+            packets.len(),
+            enqueued_ids
+        );
 
         packets
     }
@@ -65,13 +76,9 @@ impl PacketGenerator {
 
     fn generate_data(&mut self) -> Vec<u8> {
         let data_len = rand::thread_rng().gen_range(1..=100);
-        let mut data = Vec::with_capacity(data_len);
-
-        for _ in 0..data_len {
-            data.push(rand::thread_rng().gen());
-        }
-
-        data
+        rand::thread_rng().sample_iter(rand::distributions::Standard)
+            .take(data_len)
+            .collect()
     }
 
     fn generate_poisson_packets(&mut self, max_packets: usize) -> usize {
@@ -80,3 +87,7 @@ impl PacketGenerator {
         packets.min(max_packets)
     }
 }
+
+//Make an input queue of size X, outgoing ports can only send 1 packet at a time
+//for example if packet A and packet B would like to go 4, only one of them can go at a time
+//if packet A goes first, then packet B will have to wait until packet A is done. 
